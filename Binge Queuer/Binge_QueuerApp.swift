ff4e1ae965afab0b4acbdf9a824ee7a1cde8029a
Nathan Wilson <nathankishawn@gmail.com>
2025-02-11 11:45:44 +0100

    Initial Commit

---
+++ Binge Queuer/Binge_QueuerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Binge_QueuerApp.swift
+//  Binge Queuer
+//
+//  Created by Nathan Wilson on 11/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct Binge_QueuerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Binge QueuerTests/Binge_QueuerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Binge_QueuerTests.swift
+//  Binge QueuerTests
+//
+//  Created by Nathan Wilson on 11/02/2025.
+//
+
+import Testing
+@testable import Binge_Queuer
+
+struct Binge_QueuerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
