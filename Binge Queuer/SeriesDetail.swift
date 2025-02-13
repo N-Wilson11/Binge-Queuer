3dab115dee1e757e1a5821a19d9a010b2b4bb73f
Nathan Wilson <nathankishawn@gmail.com>
2025-02-13 14:48:59 +0100

    Made map view and list

---
+++ Binge Queuer/SeriesDetail.swift
@@ -0,0 +1,18 @@
+//
+//  SeriesDetail.swift
+//  Binge Queuer
+//
+//  Created by Nathan Wilson on 13/02/2025.
+//
+
+import SwiftUI
+
+struct SeriesDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SeriesDetail()
+}
